e46afbc248aff734019ccaf78807c1c3d5ca2aea
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-10-31 00:39:20 +0530

    React Basics completed

--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -1,17 +1,30 @@
 // A component file name must start with capital letter.
 
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <div>
-            <nav className="flex justify-between items-center py-5 px-6 bg-emerald-500">
+            <nav className="flex justify-between items-center py-5 px-6 bg-emerald-500 text-white">
                 
-                <h2 className="text-3xl">Rax</h2>
+                <div className="text-3xl flex gap-12">
+                    <h2>
+                        RAX
+                    </h2>
+                    <input className="rounded-2xl w-60 px-2 text-emerald-300" type="text" />
+                </div>
 
                 <div className="flex gap-9 items-center">
-                    <h5 className="text-2xl">About</h5>
-                    <h5 className="text-2xl">Contact</h5>
-                    <h5 className="text-2xl">FeedBack</h5>
-                    <h5 className="text-2xl">More</h5>
+                    {/* When usaing this the page reloads */}
+                    {/* <a className="text-2xl" href="/">Home</a>
+                    <a className="text-2xl" href="/about">About</a>
+                    <a className="text-2xl" href="/contact">Contact</a>
+                    <a className="text-2xl">More</a> */}
+
+                    {/*  */}
+                    <Link className="text-2xl" to='/'>Home</Link>
+                    <Link className="text-2xl" to='/about'>About</Link>
+                    <Link className="text-2xl" to='/contact'>Contact</Link>
                 </div>
             </nav>
         </div>
--- react/src/index.jsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-    return (
-        <div>
-            HELL
-        </div>
-    );
-}
-
-export default App;
